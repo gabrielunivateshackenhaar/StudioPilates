40350fb59a08ae07a677672a91c3e7c66e246234
Felipe Marchi <felipemarchi20@gmail.com>
2025-11-16 21:50:45 -0300

    Ajustado estilos próprios do calendário

--- view/partials/head.php
+++ view/partials/head.php
@@ -14,6 +14,7 @@
 
 <!-- CSS customizado, se houver -->
 <link rel="stylesheet" href="./css/style.css">
+<link rel="stylesheet" href="./css/calendar.css">
 
 <!-- Biblioteca fullcalendar -->
 <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.14/index.global.min.js'></script>
